d80d0a62eb2bc20983b53199e7f1086f799c83f3
Bugger-Lee <ljwl20061006@163.com>
2018-01-03 21:19:03 +0800

    evening 2018/1/3 01

---
+++ 06-definephp.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/3
+ * Time: 21:11
+ */
+?>
+<?php
+    define("Greeting","Php Is Good");
+    define("Greetings","Php Is Good",true);
+    echo Greeting,greetings;
+?>
+
