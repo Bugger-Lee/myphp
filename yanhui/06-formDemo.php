4a209e301c72aa8355e6ecc604205d1244c0226a
Bugger-Lee <ljwl20061006@163.com>
2018-01-06 23:22:15 +0800

    evening 2018/1/6 01

---
+++ yanhui/06-formDemo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/6
+ * Time: 22:40
+ */ ?>
+<form action="07-submitDemo.php" method="post">
+    姓名: <input type="text" name="username" >
+    <input type="submit" value="提交">
+</form>
+<br>
+<form action="07-submitDemo.php" method="get">
+    姓名: <input type="text" name="username" >
+    <input type="submit" value="提交">
+</form>
---
+++ yanhui/07-submitDemo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/6
+ * Time: 22:44
+ */ ?>
+<?php
+    // echo $username;
+    $a=$_POST['username'];
+    $b=$_GET['username'];
+    echo '这个学生是：'.$a;
+    echo '这个学生是：'.$b;
+?>
