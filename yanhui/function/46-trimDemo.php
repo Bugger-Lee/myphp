631fa7dda3c1fe82c57532354db146faabc66ab0
Bugger-Lee <ljwl20061006@163.com>
2018-01-17 22:03:48 +0800

    evening 2018/1/17 02

---
+++ yanhui/function/46-trimDemo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/16
+ * Time: 21:41
+ */ ?>
+<?php
+    $a = '           php         ';
+    // ltrim 清除左边空格 rtrim清除右边空格 trim清除两边空格
+    echo ltrim($a);
+    echo '<br>';
+    echo rtrim($a);
+    echo '<br>';
+    echo trim($a);
+    echo '<br>';
+?>
---
+++ yanhui/function/47-nl2brDemo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/16
+ * Time: 21:47
+ */ ?>
+<?php
+    $a = "My name is\n Lee";
+    echo $a;
+    echo '<br>';
+    // nl2br 将\n转换为br
+    echo nl2br($a);
+?>
